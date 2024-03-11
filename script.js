7851d8f1bde9776941dc2a22eb1750aa5ed16acb
vladaarta <lartwoods@yandex.ru>
2024-03-11 15:07:02 +0300

    add blocks

---
+++ script.js
@@ -0,0 +1,85 @@
+const createEl = (elTag, elClass) => {
+  const el = document.createElement(elTag);
+  if (elClass) {
+    el.classList.add(elClass);
+  }
+  return el;
+};
+
+function createRepo(data) {
+  const repoElement = createEl("li", "repos__item");
+  repoElement.innerHTML = `<p>${data.name}</p>`;
+  reposList.appendChild(repoElement);
+  repoElement.addEventListener("click", () => {
+    createRepoInfo(data);
+  });
+}
+
+const mainWrapper = createEl("div", "mainWrapper");
+const mainDiv = createEl("div");
+mainDiv.classList.add("main");
+document.body.appendChild(mainWrapper);
+mainWrapper.appendChild(mainDiv);
+document.body.style.backgroundColor = "#eee";
+
+console.log(mainDiv);
+
+const searchInput = createEl("input", "main__input");
+searchInput.type = "text";
+mainDiv.appendChild(searchInput);
+const searchSpan = createEl("span", "main__searchSpan");
+mainDiv.appendChild(searchSpan);
+const repos = createEl("div", "repos");
+const reposList = createEl("ul", "repos__list");
+mainDiv.appendChild(repos);
+repos.appendChild(reposList);
+const reposInfo = createEl("div", "repos-info");
+const reposInfoList = createEl("div", "repos-info__list");
+mainDiv.appendChild(reposInfo);
+reposInfo.appendChild(reposInfoList);
+
+const repo_per_page = 5;
+
+async function searchRepos() {
+  const inputValue = searchInput.value.trim();
+  if (inputValue === "") {
+    reposList.innerHTML = "";
+    reposList.style.display = "none";
+    return;
+  }
+
+  try {
+    const response = await fetch(
+      `https://api.github.com/search/repositories?q=${inputValue}&per_page=${repo_per_page}`
+    );
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const responseData = await response.json();
+    reposList.innerHTML = "";
+    responseData.items.forEach((repo) => createRepo(repo));
+    reposList.style.display = "block";
+  } catch (error) {
+    console.error("Fetch error:", error);
+  }
+}
+const debounce = (fn, debounceTime) => {
+  let timeout;
+  return function () {
+    const fnCall = () => {
+      fn.apply(this, arguments);
+    };
+    clearTimeout(timeout);
+    timeout = setTimeout(fnCall, debounceTime);
+  };
+};
+
+searchInput.addEventListener("keyup", debounce(searchRepos, 400));
+function createRepoInfo(data) {
+  const repoInfo = createEl("div", "repos__info");
+  repoInfo.innerHTML = `<div><p>Name:</p><p>${data.name}</p></div>
+  <div><p>Owner:</p><p>${data.owner.login}</p></div>
+  <div><p>Stars:</p><p>${data.stargazers_count}</p></div>`;
+  reposInfo.appendChild(repoInfo);
+}
